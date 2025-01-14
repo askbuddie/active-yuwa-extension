5831757c993d6283d04f7bda24cc279eced2b484
Ashish Yadav <18111862+ashiishme@users.noreply.github.com>
2025-01-14 18:16:29 +0100

    :wrench: configure tailwind & initial chrome extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <>
-      <h1>Active Yuwa Extension</h1>
-    </>
-  )
+    <h1 className="my-5 text-center text-3xl font-bold">
+      Active Yuwa Extension
+    </h1>
+  );
 }
 
-export default App
+export default App;
