e40f1d1921130ad77b447a8cdadb7c9ce67d71f4
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2025-01-18 12:28:52 +0000

    delete extra popup html file (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
   return (
-    <h1 className="my-5 text-center text-3xl font-bold">
-      Active Yuwa Extension
-    </h1>
+    <main>
+      <h1 className="my-5 text-center text-3xl font-bold">
+        Active Yuwa Extension
+      </h1>
+      <button id="runScript">Fetch Low-Comment Posts</button>
+      <ul id="postList"></ul>
+      <p id="noPosts" style={{ display: "none" }}>No low-comment posts found!</p>
+    </main>
   );
 }
 
